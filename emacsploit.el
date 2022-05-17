29471a19fe6ed25eb8573532f5918739dca046a0
4shen0ne <4shen.01@gmail.com>
2022-05-17 11:09:16 +0800

    ♻️ rename symbols

--- emacsploit.el
+++ emacsploit.el
@@ -1,4 +1,4 @@
-;;; emacsploit.el --- search in exploit-db -*- lexical-binding: t; -*-
+;;; emacsploit.el --- Emacs client for Exploit-DB -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2022 4shen0ne
 ;;
@@ -9,7 +9,7 @@
 ;;
 ;;; Commentary:
 ;;
-;;  search in exploit-db
+;;  Emacs client for Exploit-DB
 ;;
 ;;; Code:
 
@@ -25,44 +25,52 @@
 Used to select appropriate mode from fetched exploit (based on class)."
   )
 
-(defvar emacsploit-list-mode-map
+(defvar emacsploit-table-mode-map
   (let ((map (make-sparse-keymap)))
     (set-keymap-parent map tabulated-list-mode-map)
-    (define-key map "o" 'emacsploit-open-current)
+    (define-key map (kbd "<return>") 'emacsploit-open-current)
+    (define-key map (kbd "<C-return>") 'emacsploit-browse-current)
     map))
 
 (defun emacsploit-open-current ()
   (interactive)
-  (let ((id (and (eq major-mode 'emacsploit-list-mode)
+  (let ((id (and (eq major-mode 'emacsploit-table-mode)
                  (tabulated-list-get-id))))
     (emacsploit-search-by-id (string-to-number id))))
 
-(define-derived-mode emacsploit-list-mode tabulated-list-mode "Exploit-db"
+(defun emacsploit-browse-current ()
+  (interactive)
+  (let ((id (and (eq major-mode 'emacsploit-table-mode)
+                 (tabulated-list-get-id))))
+    (emacsploit-browse-by-id (string-to-number id))))
+
+(define-derived-mode emacsploit-table-mode tabulated-list-mode "Exploit-db"
   (setq tabulated-list-format [("Date" 12 nil) ("Type" 10 nil) ("Title" 0 nil)]
         tabulated-list-padding 2
         tabulated-list-sort-key nil)
   (tabulated-list-init-header))
 
-(defun emacsploit-tabulated-entry (data)
+(defun emacsploit--tabulated-entry (data)
   (let* ((desc (cdr (assoc 'description data)))
          (edb-id (aref desc 0))
          (title (aref desc 1))
          (date (cdr (assoc 'date_published data)))
          (type (cdr (assoc 'type_id data))))
-    (list edb-id (vector date type (emacsploit-decode-entities title)))))
+    (list edb-id (vector date type (emacsploit--decode-entities title)))))
 
-(defun emacsploit-decode-entities (html)
+(defun emacsploit--decode-entities (html)
   (with-temp-buffer
     (save-excursion (insert html))
     (xml-parse-string)))
 
 ;;;###autoload
 (defun emacsploit-search-by-title (text)
+  "Search exploits by title, then show the results table.
+Display columns: Date | Type | Title"
   (interactive "MTitle: ")
 
  (let* ((exp-buffer-name (format "*exploit %s*" text))
         (exp-buffer (get-buffer exp-buffer-name))
-        ;; (url-mime-accept-string "application/json, text/javascript, */*; q=0.01")
         (request-params
          (emacsploit-make-query-string
           `(("columns[0][data]" . "date_published")
@@ -105,9 +113,9 @@ Used to select appropriate mode from fetched exploit (based on class)."
          (rename-buffer exp-buffer-name t)
          (goto-char url-http-end-of-headers)
          ;; (delete-region (point-min) (point))
-         (emacsploit-list-mode)
+         (emacsploit-table-mode)
          (setq tabulated-list-entries
-               (mapcar 'emacsploit-tabulated-entry
+               (mapcar 'emacsploit--tabulated-entry
                        (cdr (assoc 'data (json-read)))))
          (tabulated-list-print)
          (set-buffer-modified-p nil))
@@ -122,7 +130,7 @@ Used to select appropriate mode from fetched exploit (based on class)."
    params "&"))
 
 (defun emacsploit-search-by-id (edb-id)
-  "Search exp from exploit-db by EDB-ID."
+  "Fetch the exploit code by EDB-ID."
   (interactive "nEDB-ID: ")
 
   (let* ((exp-buffer-name (format "*exploit %d*" edb-id))
@@ -153,6 +161,10 @@ Used to select appropriate mode from fetched exploit (based on class)."
 
           (set-buffer-modified-p nil))
         (switch-to-buffer-other-window exp-buffer)))))
+
+(defun emacsploit-browse-by-id (edb-id)
+  (interactive "nEDB-ID: ")
+  (browse-url (format "https://www.exploit-db.com/exploits/%d" edb-id)))
 
 (provide 'emacsploit)
 ;;; emacsploit.el ends here
